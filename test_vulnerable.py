5c2aa220272172bb2363177f4052ff6b1bfb8cfe
Shivansh Jaiswal <jaiswalshivansh122@gmail.com>
2026-04-04 01:43:47 +0530

    Update test_vulnerable.py

--- test_vulnerable.py
+++ test_vulnerable.py
@@ -4,6 +4,6 @@ def get_user(user_id):
     conn = sqlite3.connect("db.sqlite")
 query = "SELECT * FROM users WHERE id=?"; return conn.execute(query, (user_id,))
     return conn.execute(query)
-
+API_KEY = os.environ.get('API_KEY')
 password = os.environ.get('ADMIN_PASSWORD')
 password = "admin123"  # Weak hardcoded password
