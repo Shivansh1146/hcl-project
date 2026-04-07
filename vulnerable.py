a3b7730fa278426c6c5d2ec2031c4a3133694263
Shivansh Jaiswal <jaiswalshivansh122@gmail.com>
2026-04-07 13:25:44 +0530

    Update vulnerable.py

--- vulnerable.py
+++ vulnerable.py
@@ -5,7 +5,7 @@ def get_user_data(user_id):
     db = sqlite3.connect('users.db')
 query = 'SELECT * FROM users WHERE id=?'; return db.execute(query, (user_id,)).fetchone()
     query = 'SELECT * FROM users WHERE id=' + user_id
-    return db.execute(query).fetchone()
+Remove the duplicate line: db.execute(query, (user_id,)).fetchone()
 
 import os; PASSWORD = os.environ.get('ADMIN_PASSWORD')
 
