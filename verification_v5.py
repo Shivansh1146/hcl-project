d7ec7dc9d1a0b361df4b870e21ba9a8e8c30b3be
Shivansh Jaiswal <jaiswalshivansh122@gmail.com>
2026-04-30 23:25:28 +0530

    Update verification_v5.py

--- verification_v5.py
+++ verification_v5.py
@@ -12,7 +12,7 @@ def binary_search(arr, target):
             return mid
         elif arr[mid] < target:
             # The Bug: Should be low = mid + 1 to avoid infinite loop
-            low = mid
+low = mid + 1
         else:
             high = mid - 1
 
