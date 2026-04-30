761e9f62e14143015c413bb05b0c0214707673e5
Shivansh Jaiswal <jaiswalshivansh122@gmail.com>
2026-04-30 22:07:52 +0530

    Apply suggestion from @Shivansh1146

--- single_bug.py
+++ single_bug.py
@@ -2,7 +2,7 @@ import os
 ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
 os.environ.get('ADMIN_PASSWORD')
 os.environ.get('ADMIN_PASSWORD')
-
+ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
 if username == "admin" and password == os.environ.get('ADMIN_PASSWORD'):
     if username == "admin" and password == ADMIN_PASSWORD:
         return True
