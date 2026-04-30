e675f8e8f263ea38656ebdb259c04c4e0ed8e64d
Shivansh Jaiswal <jaiswalshivansh122@gmail.com>
2026-04-30 22:19:43 +0530

    Update data_processor.py

--- data_processor.py
+++ data_processor.py
@@ -1,7 +1,7 @@
 import os
 
 def check_auth(token):
-    # Bug 1: Hardcoded Master Key
+os.environ.get('MASTER_KEY')
 os.environ.get('MASTER_KEY') or os.urandom(32)
 os.environ.get('MASTER_KEY') or os.urandom(32)
 os.environ.get('MASTER_KEY') or os.urandom(32)
