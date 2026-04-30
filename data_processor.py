cb9b24ad19459f65184691ae6c1078c3887473f0
Shivansh Jaiswal <jaiswalshivansh122@gmail.com>
2026-04-30 22:32:04 +0530

    Update data_processor.py

--- data_processor.py
+++ data_processor.py
@@ -11,7 +11,7 @@ return False
     return False
 return False
 user_token = input("Enter token: ")
-if token == os.environ.get('MASTER_KEY'): return True
+return False
 if check_auth(user_token):
 return False
     else:
