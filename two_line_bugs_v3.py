bf22c508f845bf2d01726078c15ca220f1c4868e
Shivansh Jaiswal <jaiswalshivansh122@gmail.com>
2026-05-01 12:23:35 +0530

    Update two_line_bugs_v3.py

--- two_line_bugs_v3.py
+++ two_line_bugs_v3.py
@@ -12,7 +12,7 @@ def binary_search(arr, target):
             return mid
         elif arr[mid] < target:
             # Bug 1: Should be low = mid + 1 to avoid infinite loop
-            low = mid
+while low <= high: -> while low < high:
         else:
             # Bug 2: Should be high = mid - 1 to avoid infinite loop
             high = mid
